0de86600f5a40606cc4a331db2ae398b32c83916
colepalm <palm.cole@gmail.com>
2021-08-11 21:05:53 -0600

    zig zag progress

--- zigzag-conversion.js
+++ zigzag-conversion.js
@@ -4,22 +4,42 @@
  * @return {string}
  */
 var convert = function(s, numRows) {
-    let matrix = new Array(numRows).fill(0).map(() => new Array(1).fill('_'));
+    let matrix = new Array(numRows).fill(0).map(() => new Array(1).fill(''));
     let currentRow = 0, currentColumn = 0;
     for (let i = 0; i < s.length; i++) {
         if (currentRow === numRows - 1) {
             matrix[currentRow][currentColumn] = s.charAt(i++)
-            while(currentRow > 0) {
-                matrix[--currentRow][++currentColumn] = s.charAt(i++);
+            if (numRows > 1) {
+                while(currentRow > 0) {
+                    matrix[--currentRow][++currentColumn] = s.charAt(i++);
+                }
+                matrix[++currentRow][++currentColumn] = s.charAt(i);
+                currentRow++;
+            } else {
+                while(i < s.length) {
+                    matrix[currentRow][++currentColumn] = s.charAt(i++);
+
+                }
             }
-            currentRow++;
-            currentColumn++;
         } else {
-            matrix[currentRow++][currentColumn] = s.charAt(i)
+            matrix[currentRow++][currentColumn] = s.charAt(i);
         }
     }
 
-    console.log(matrix)
+    let output = '';
+    for (let i = 0; i < numRows; i++) {
+        matrix[i].forEach(letter => {
+            output += letter
+        })
+    }
+
+    return output;
 };
 
-convert("PAYPALISHIRING", 4)
+// console.log(convert("PAYPALISHIRING", 3))
+// console.log(convert("PAYPALISHIRING", 4))
+// console.log(convert("A", 1))
+// console.log(convert("AB", 1))
+// console.log(convert("ABC", 1))
+console.log(convert("ABCDE", 2))
+
