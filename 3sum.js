5cf6ad78716ffbe9deda33536997d311c5f8aa22
colepalm <palm.cole@gmail.com>
2022-03-16 20:14:04 -0600

    3sum-closest

--- 3sum.js
+++ 3sum.js
@@ -10,7 +10,7 @@ var threeSum = function(nums) {
 
     for (let i = 0; i < sorted.length; i++) {
         if (sorted[i - 1] && sorted[i - 1] === sorted[i]) {
-            break;
+            continue;
         }
 
         let reverseSubtract = 0;
@@ -38,9 +38,10 @@ var threeSum = function(nums) {
     return triplets;
 };
 
-console.log(threeSum([-1,0,1,2,-1,-4]))
-console.log(threeSum([]))
-console.log(threeSum([0]))
-console.log(threeSum([0,0,0]))
-console.log(threeSum([0,0,0,0]))
-console.log(threeSum([1,2,-2,-1]))
+// console.log(threeSum([-1,0,1,2,-1,-4]))
+// console.log(threeSum([]))
+// console.log(threeSum([0]))
+// console.log(threeSum([0,0,0]))
+// console.log(threeSum([0,0,0,0]))
+// console.log(threeSum([1,2,-2,-1]))
+console.log(threeSum([34,55,79,28,46,33,2,48,31,-3,84,71,52,-3,93,15,21,-43,57,-6,86,56,94,74,83,-14,28,-66,46,-49,62,-11,43,65,77,12,47,61,26,1,13,29,55,-82,76,26,15,-29,36,-29,10,-70,69,17,49]))
