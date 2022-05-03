6b85b88de08bf6eea1b02348c47a208700b42d2e
colepalm <palm.cole@gmail.com>
2022-05-02 21:32:53 -0600

    3sum-closest

--- 3sum-closest.js
+++ 3sum-closest.js
@@ -7,67 +7,31 @@ function threeSumClosest(nums, target) {
     // afford the O(NlogN) sort operation
     nums = nums.sort((a, b) => a - b)
 
-    // if the question asks us for a custom target, we can control it here
-    let closest = 99999;
+    let closest = Infinity;
 
     for (let i = 0; i < nums.length - 2; i++) {
-        // `i` represents the "left" most number in our sorted set.
-        // once this number hits 0, there's no need to go further since
-        // positive numbers cannot sum to a negative number
-        // if (nums[i] > target) break
-
-        // we don't want repeats, so skip numbers we've already seen
         if (i > 0 && nums[i] === nums[i - 1]) continue
 
-        // `j` represents the "middle" element between `i` and `k`.
-        // we will increment this up through the array while `i` and `k`
-        // are anchored to their positions. we will decrement `k` for
-        // for each pass through the array, and finally increment `i`
-        // once `j` and `k` meet.
         let j = i + 1
 
-        // `k` represents the "right" most element
         let k = nums.length - 1
-
-        // to summarize our setup, we have `i` that starts at the beginning,
-        // `k` that starts at the end, and `j` that races in between the two.
-        //
-        // note that `i` is controlled by our outer for-loop and will move the slowest.
-        // in the meantime, `j` and `k` will take turns inching towards each other depending
-        // on some logic we'll set up below. once they collide, `i` will be incremented up
-        // and we'll repeat the process.
 
         while (j < k) {
             let sum = nums[i] + nums[j] + nums[k]
+            if (sum === target) return target
 
-            // if we find the target sum, increment `j` and decrement `k` for
-            // other potential combos where `i` is the anchor
-            if ((Math.abs(sum - target)) < (Math.abs(closest - target))) {
-                // store the valid threesum
-                closest = sum;
+            closest = Math.abs(sum - target) < Math.abs(closest - target) ? sum : closest
 
-                // this is important! we need to continue to increment `j` and decrement `k`
-                // as long as those values are duplicated. in other words, we wanna skip values
-                // we've already seen. otherwise, an input array of [-2,0,0,2,2] would result in
-                // [[-2,0,2], [-2,0,2]].
-                //
-                // (i'm not a fan of this part because we're doing a while loop as we're
-                // already inside of another while loop...)
-                while (nums[j] === nums[j + 1]) j++
-                while (nums[k] === nums[k - 1]) k--
-
-                // finally, we need to actually move `j` forward and `k` backward to the
-                // next unique elements. the previous while loops will not handle this.
-                j++
-                k--
-
-                // if the sum is too small, increment `j` to get closer to the target
-            } else if (sum < target) {
-                j++
-
-                // if the sum is too large, decrement `k` to get closer to the target
-            } else { // (sum > target)
-                k--
+            if (sum < target) {
+                j++;
+                while (j < k && nums[j] === nums[j - 1]) {
+                    j++;
+                }
+            } else {
+                k--;
+                while (j < k && nums[k] === nums[k+1]) {
+                    k--;
+                }
             }
         }
     }
