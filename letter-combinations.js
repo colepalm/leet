ca6ef89cd2849ab4e34f449d8433099627f10823
colepalm <palm.cole@gmail.com>
2022-07-07 19:59:53 -0600

    (WIP) letter-combinations

--- letter-combinations.js
+++ letter-combinations.js
@@ -3,18 +3,29 @@
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
+    // stores all combinations
     let combinations = [];
+    // maintains current level position
     let letterPosition = 0;
+    // number of times each digit needs to get printed
+    let print = 3 ** (digits.length - 1);
     for (let i = 0; i < digits.length; i++) {
+        // current position on number
         letterPosition = 0;
+        // every number has 3 letters
         let count = 2;
         while(count > -1) {
-            const actualNumber = (digits[i] - 1) * 3;
+            // tracks max number of character on a number pad digit
+            const actualNumber = (digits[i] - 1) * 3 - 1;
             const letter = alphabet.charAt(actualNumber - count)
             if (i === 0) {
-                combinations.push(letter)
+                for (let j = 0; j < print; j++) {
+                    combinations.push(letter)
+                }
             } else {
-                combinations[letterPosition] = combinations[letterPosition] += letter;
+                for (let j = 0; j < print; j++) {
+                    combinations[letterPosition + (j * 3)] = combinations[letterPosition + (j * 3)] += letter;
+                }
                 letterPosition++;
             }
             count--;
