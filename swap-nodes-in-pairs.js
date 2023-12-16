3ac6f67ab94848ea01a6ae2bd4024db5483fb49e
colepalm <palm.cole@gmail.com>
2023-12-16 13:10:55 -0700

    swap-nodes-in-pairs

--- swap-nodes-in-pairs.js
+++ swap-nodes-in-pairs.js
@@ -11,7 +11,7 @@
  */
 var swapPairs = function(head) {
     let newHead = head;
-    if (head.next) {
+    if (head && head.next) {
         newHead = head.next;
         process(head)
         return newHead;
@@ -38,7 +38,7 @@ var process = function(node) {
 };
 
 const list = {"val":1,"next":{"val": 2, next: {"val":3,"next":{"val":4,"next":null}}}}
-const list2 = {};
+const list2 = null;
 const list3 = {"val": 1}
 const newlist = swapPairs(list)
 const newlist2 = swapPairs(list2)
