af90f4a58c2b4e91857fd01031b1d381f33aeb32
colepalm <palm.cole@gmail.com>
2025-08-07 21:42:13 -0600

    jump-game-ii

---
+++ jump-game-ii.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function(nums) {
+    const length = nums.length;
+    const index = 0;
+    while (length > index) {
+        for (let i = 0; i < length; i++) {}
+    }
+};
