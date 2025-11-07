a3925fe2e64814b4b40b952f1707ecd5f7d18956
colepalm <palm.cole@gmail.com>
2025-11-06 21:15:51 -0700

    divide-two-integers

--- divide-two-integers.js
+++ divide-two-integers.js
@@ -5,51 +5,36 @@
  */
 var divide = function(dividend, divisor) {
     if (dividend === 0) { return 0; }
-    if (dividend === divisor) { return 1; }
-    if (dividend === -divisor) { return -1; }
-    if (divisor === 1 || divisor === -1) {
-        if (divisor < 0 && dividend < 0) { return -dividend-1 }
-        else if (divisor > 0 && dividend > 0) { return dividend }
-        else if (dividend < 0) { return dividend; }
-        else return -dividend
+    let isNegative = false;
+    if ((dividend > 0 && divisor < 0) || (dividend < 0 && divisor > 0)) { isNegative = true; }
+
+    dividend = Math.abs(dividend);
+    divisor = Math.abs(divisor);
+
+    if (dividend === divisor) { if (isNegative) { return -1; } }
+
+    let count = 0
+    while (divisor <= dividend) {
+        let value = divisor
+        let multiple = 1
+        while (value + value <= dividend) {
+            value += value
+            multiple += multiple
+        }
+        dividend = dividend - value
+        count += multiple
     }
-    if (dividend < 0 && divisor < 0) {
-        return positiveNumbers(dividend, divisor)
-    } else if (dividend > 0 && divisor > 0) {
-        return positiveNumbers(dividend, divisor)
-    } else return negativeNumbers(dividend, divisor)
+
+    if (count > ((2**31) - 1)) {
+        return isNegative ? -(2**31) : 2**31 - 1
+    }
+
+    if (isNegative) { return -count; }
+    return count;
 };
-
-var positiveNumbers = function(dividend, divisor) {
-    let count = 0;
-    if (dividend < 0) {
-        dividend = -dividend;
-        divisor = -divisor
-    }
-    let total = divisor;
-    while (total <= dividend) {
-        count++;
-        total = total + divisor;
-    }
-    return count;
-}
-
-var negativeNumbers = function(dividend, divisor) {
-    let count = 0;
-    if (dividend < 0) {
-        divisor = -divisor;
-    } else {
-        dividend = -dividend
-    }
-    let total = divisor;
-    while (total >= dividend) {
-        count--;
-        total = total + divisor;
-    }
-    return count;
-}
 
 // console.log(divide(10, 3))
 // console.log(divide(7, -3))
 // console.log(divide(-2147483648, -1))
-console.log(divide(-2147483648, 2))
+// console.log(divide(521521, -3))
+console.log(divide(1, -1))
